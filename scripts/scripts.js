b0a9e428c4fdf6c6652bb7e8995c58bf59879b44
Rojay Arinze <rarinze1@ncstudents.niagaracollege.ca>
2024-10-27 23:23:54 -0400

    add toastr notifications

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -29,3 +29,44 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
   });
+
+
+  //TOASTS
+  $(document).ready(function() {
+    // Toastr options
+    toastr.options = {
+        "closeButton": true,
+        "debug": false,
+        "newestOnTop": false,
+        "progressBar": true,
+        "positionClass": "toast-top-right",
+        "preventDuplicates": false,
+        "onclick": null,
+        "showDuration": "300",
+        "hideDuration": "1000",
+        "timeOut": "5000",
+        "extendedTimeOut": "1000",
+        "showEasing": "swing",
+        "hideEasing": "linear",
+        "showMethod": "fadeIn",
+        "hideMethod": "fadeOut"
+    };
+
+    // Button click event
+      // Button click event for form submission
+      $('#ncr-form').on('submit', function(e) {
+        e.preventDefault(); // Prevent form submission
+        toastr.success('Success!', 'You have successfully created a new NCR');
+    });
+
+    // Clear All button functionality
+    $('#btnClearAll').on('click', function() {
+        $('#ncr-form')[0].reset(); // Clear all form fields
+        toastr.error('Cleared!', 'All fields have been cleared');
+    });
+
+    // Cancel button event
+    $('#btnCancel').on('click', function() {
+        toastr.error('Canceled!', 'You have canceled creating a new NCR');
+    });
+});
